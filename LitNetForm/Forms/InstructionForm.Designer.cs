7a26bc8b8d0a4f05743ac6c017338224d6ff732c
knyukh_av <124026869+AlVK04@users.noreply.github.com>
2025-12-17 15:18:05 +0300

    Добавлены новые иконки

--- LitNetForm/Forms/InstructionForm.Designer.cs
+++ LitNetForm/Forms/InstructionForm.Designer.cs
@@ -45,57 +45,64 @@
             tabControl1.Controls.Add(tabPage5);
             tabControl1.Dock = DockStyle.Fill;
             tabControl1.Location = new Point(0, 0);
+            tabControl1.Margin = new Padding(3, 4, 3, 4);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(800, 450);
+            tabControl1.Size = new Size(914, 600);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            tabPage1.Location = new Point(4, 24);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Margin = new Padding(3, 4, 3, 4);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(792, 422);
+            tabPage1.Padding = new Padding(3, 4, 3, 4);
+            tabPage1.Size = new Size(906, 567);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "О программе";
             tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(4, 24);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Margin = new Padding(3, 4, 3, 4);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(792, 422);
+            tabPage2.Padding = new Padding(3, 4, 3, 4);
+            tabPage2.Size = new Size(906, 567);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Интерфейс";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            tabPage4.Location = new Point(4, 24);
+            tabPage4.Location = new Point(4, 29);
+            tabPage4.Margin = new Padding(3, 4, 3, 4);
             tabPage4.Name = "tabPage4";
-            tabPage4.Size = new Size(792, 422);
+            tabPage4.Size = new Size(906, 567);
             tabPage4.TabIndex = 3;
             tabPage4.Text = "Алгоритм работы";
             tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
-            tabPage5.Location = new Point(4, 24);
+            tabPage5.Location = new Point(4, 29);
+            tabPage5.Margin = new Padding(3, 4, 3, 4);
             tabPage5.Name = "tabPage5";
-            tabPage5.Size = new Size(792, 422);
+            tabPage5.Size = new Size(906, 567);
             tabPage5.TabIndex = 4;
             tabPage5.Text = "Частые вопросы (FAQ)";
             tabPage5.UseVisualStyleBackColor = true;
             // 
             // InstructionForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(tabControl1);
             Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 4, 3, 4);
             Name = "InstructionForm";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Инструкция";
             tabControl1.ResumeLayout(false);
             ResumeLayout(false);

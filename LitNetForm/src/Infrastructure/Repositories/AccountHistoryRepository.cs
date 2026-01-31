6bd76ac6fb2d3d253b625a23b88e59ae644eb1ef
Nikita Petrunyak <ox1om2002@gmail.com>
2026-01-31 17:32:06 +0300

    метод сохранения истории действий

--- LitNetForm/src/Infrastructure/Repositories/AccountHistoryRepository.cs
+++ LitNetForm/src/Infrastructure/Repositories/AccountHistoryRepository.cs
@@ -1,11 +1,21 @@
 ﻿using Core.Abstracts;
 using Core.Entities;
+using Infrastructure.Database;
 
 namespace Infrastructure.Repositories;
 
-public class AccountHistoryRepository : IAccountHistoryRepository
+public class AccountHistoryRepository(AppDbContext dbContext) : IAccountHistoryRepository
 {
     public async Task AddActionAsync(AccountActionHistory accountActionHistory, CancellationToken cancellationToken)
     {
+        try
+        {
+            await dbContext.AccountActionHistories.AddAsync(accountActionHistory, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
+        }
+        catch (Exception)
+        {
+            
+        }
     }
 }
